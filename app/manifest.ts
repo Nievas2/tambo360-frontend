9d9e5c39ebdcf0102bee15a3ce10311f37c70f9b
Nievas2 <angelgabrielnievas@gmail.com>
2026-04-06 14:12:40 -0300

    feat(Manifest): :sparkles: add manifest id

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,6 +2,7 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
+    id: '/',
     name: 'Tambo360',
     short_name: 'Tambo',
     description:
