9c3b7660f6264c6ebb63396bfcbd5193277cda05
Nievas2 <angelgabrielnievas@gmail.com>
2026-04-09 11:01:13 -0300

    style(Landing): :lipstick: Change hero styles

--- components/shared/landing/Hero.tsx
+++ components/shared/landing/Hero.tsx
@@ -5,31 +5,30 @@ import Link from 'next/link'
 
 const Hero = () => {
   return (
-    <section className="pt-28 pb-20 px-6 bg-[url('/vacas_1.webp')] bg-center bg-cover bg-no-repeat flex items-center justify-center">
-      <div className="max-w-6xl mx-auto flex items-center justify-center">
+    <section className="pt-28 pb-20 px-6 bg-[url('/vacas_1.webp')] bg-center bg-cover bg-no-repeat flex items-center min-h-[85vh]">
+      <div className="max-w-6xl flex md:px-20">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }}
-          className="max-w-2xl flex flex-col items-center justify-center"
+          className="max-w-2xl flex flex-col items-center"
         >
-          <h1 className="text-4xl md:text-5xl text-center font-extrabold text-white leading-tight mb-5">
-            Simplicidad operativa <span className="">para tu tambo.</span>
+          <h1 className="text-[40px] font-extrabold text-white leading-tight mb-5 text-center md:text-start">
+            Tu tambo bajo control, en tiempo real
           </h1>
 
-          <p className="text-white text-lg leading-relaxed mb-8 max-w-xl text-center">
-            Digitaliza el registro de producción, controla mermas y visualiza
-            los costos reales de tu tambo mediante ingeniería orientada al
-            campo.
+          <p className="text-white text-lg leading-relaxed mb-8 text-center md:text-start">
+            Deja de gestionar a ciegas: producción, mermas y costos en un solo
+            sistema pensado para el campo.
           </p>
 
-          <div className="flex flex-wrap gap-4">
-            <Button variant="default" className="h-12 w-32" asChild>
-              <Link href="/iniciar-sesion">Ver Demo</Link>
+          <div className="flex gap-4 justify-center md:justify-start w-full">
+            <Button variant="landing" className="h-12 w-44">
+              Ver Demo
             </Button>
 
-            <Button variant="outline" className="h-12 w-32" asChild>
-              <Link href="/producto">Conocer más</Link>
+            <Button variant="landingSecondary" className="h-12 w-44" asChild>
+              <Link href="/iniciar-sesion">Probar ahora</Link>
             </Button>
           </div>
         </motion.div>
