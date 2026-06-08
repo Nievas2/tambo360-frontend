ce83a1c52db86190dc68616184277d198a296f22
Nievas2 <angelgabrielnievas@gmail.com>
2026-06-08 11:54:45 -0300

    fix(Organization): :bug: fix onboard flow

--- hooks/organization/useCreateOrganization.ts
+++ hooks/organization/useCreateOrganization.ts
@@ -1,11 +1,21 @@
+'use client'
+import { useAuth } from '@/context/AuthContext'
 import { createOrganization } from '@/utils/api/organization.api'
 import { useMutation } from '@tanstack/react-query'
 import { AxiosError, AxiosResponse } from 'axios'
 
 export function useCreateOrganization() {
+  const { setUser } = useAuth()
   return useMutation<AxiosResponse, AxiosError<{ message: string }>, string>({
     mutationFn: async (name: string) => {
       const { data } = await createOrganization(name)
+      setUser((prevUser) => {
+        if (!prevUser) return prevUser
+        return {
+          ...prevUser,
+          organizaciones: data,
+        }
+      })
       return data
     },
   })
